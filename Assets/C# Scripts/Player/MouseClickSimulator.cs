7b852d8ae8576d8d6459d44855368371d8a1cbdd
SE-YANG <2217705870@qq.com>
2024-02-19 16:10:32 +0800

    将鼠标锁到屏幕中央，模拟鼠标点击事件

---
+++ Assets/C# Scripts/Player/MouseClickSimulator.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//使用Cursor.lockState = CursorLockMode.Locked后
+//鼠标位置被固定在屏幕中央并且不知道为什么不能进行点击事件
+//故请出ChatGPT来帮忙写出此脚本手动模拟点击事件
+public class MouseClickSimulator : MonoBehaviour
+{
+    void Update()
+    {
+        // 模拟左键点击
+        if (Input.GetMouseButtonDown(0))
+        {
+            SimulateMouseClick();
+        }
+    }
+
+    void SimulateMouseClick()
+    {
+        // 获取当前鼠标位置
+        Vector2 mousePosition = Input.mousePosition;
+
+        // 创建PointerEventData
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+
+        // 设置鼠标位置
+        pointerEventData.position = Input.mousePosition;
+        
+        // 获取点击到的所有对象
+        var raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+
+        // 遍历点击到的对象并执行点击事件
+        foreach (var hit in raycastResults)
+        {
+            ExecuteEvents.Execute(hit.gameObject, pointerEventData, ExecuteEvents.pointerClickHandler);
+        }
+    }
+}
+
