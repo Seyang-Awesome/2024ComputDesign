de85bb4964c1d7863899d5c9a5313d8fb5f0840e
Maric <1109361945@qq.com>
2024-02-19 16:44:26 +0800

    上传一下材质，代码目前还有问题

---
+++ Assets/C# Scripts/Arithmetic/DecimalismUI.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using DG.DemiLib;
+
+public class DecimalismUI : Publisher
+{
+    [SerializeField] private TextMeshProUGUI lengthText;
+    [SerializeField] private Button lengthUpButton;
+    [SerializeField] private Button lengthDownButton;
+    public List<MoveBlock> blocks = new();
+    private List<int> lengthList;//双向链表
+    private int currentLength;
+
+
+    private float lengthFontSize;
+    private float dimensionFontSize;
+    
+    private void Start()
+    {
+        lengthList = new(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10 });
+        currentLength = lengthList[0];
+
+        lengthUpButton.onClick.AddListener(OnClickLengthUpButton);
+        lengthDownButton.onClick.AddListener(OnClickLengthDownButton);
+
+        lengthFontSize = lengthText.fontSize;
+        
+    }
+
+    private void OnClickLengthUpButton()
+    {
+        Debug.Log("按上键");
+        blocks[currentLength].MoveToOrigin();
+        if (currentLength == 0)
+        {
+            currentLength = 10;
+        }
+        else
+        {
+            currentLength--;
+        }
+
+        ShowLengthText();
+        
+    }
+    
+    private void OnClickLengthDownButton()
+    {
+        Debug.Log("按下键");
+        currentLength = currentLength.Next ?? lengthList.First;
+        if (currentLength == 10)
+        {
+            currentLength = 0;
+        }
+        else
+        {
+            currentLength++;
+        }
+        blocks[currentLength.Value].MoveToTarget();
+        ShowLengthText();
+    }
+    
+
+    private void ShowLengthText()
+    {
+        lengthText.DOBounceFontSize(lengthFontSize, currentLength.Value.ToString(), Consts.UITextChangeDuration);
+    }
+    
+    
+}
+
